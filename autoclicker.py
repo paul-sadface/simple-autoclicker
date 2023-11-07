3cc01cf8c244c6c5115910ef93b6b6eb882c3b06
paul-sadface <paultriminh60@gmail.com>
2023-11-06 23:48:46 -0500

    create a structure for the program. Also created the method create-window which creates the tkinter window. Still in progress.

--- autoclicker.py
+++ autoclicker.py
@@ -1,8 +1,27 @@
 import pyautogui
 from pynput.keyboard import *
+from tkinter import *
+from tkinter import ttk
+
+class autoclicker():
+    def __init__(self):
+        self.create_window()
+
+    def create_window(self):
+        root = Tk()
+        root.title("Autoclicker Program")
+        frame = ttk.Frame(root, padding=20)
+        frame.grid(column=0, row=0)
+        root.geometry("500x300")
+        ttk.Label(frame, text="Autoclicker Program",).grid(column=0, row=0)
+        ttk.Entry()
+        ttk.Button(frame, text="Close", command=root.destroy).grid(column=0, row=1)
+        root.mainloop()
+
+
 
 def main():
-    pass
+    main_autoclicker = autoclicker()
 
 
 if __name__ == "__main__":
