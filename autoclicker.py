35d4d93efea40b3d341e8db4b19f3538aee7f77a
paul-sadface <paultriminh60@gmail.com>
2023-11-29 22:47:14 -0500

    Restructure the main and init parts of the code. Also created a stop_autoclicker method to use once a button is pressed to stop running. Added a label which can be edited to include messages such as 'success' or errors (e.g. 'inputted a string instead of an int')

--- autoclicker.py
+++ autoclicker.py
@@ -7,34 +7,43 @@ import time
 
 class autoclicker():
     def __init__(self):
-        self.create_window()
+        self.is_running = False
+        self.root = None
 
     def get_frequency(self):
-
-        print(self.frequency_string.get())
+        self.frequency = self.frequency_string.get()
+        print(self.frequency)
+        self.ui_feedback_label.configure(text="success")
 
     def create_window(self):
-        root = Tk()
-        root.title("Autoclicker Program")
-        frame = ttk.Frame(root, padding=20)
+        self.root = Tk()
+        self.root.title("Autoclicker Program")
+        frame = ttk.Frame(self.root, padding=20)
         frame.grid(column=0, row=0)
-        root.geometry("500x300")
-        self.frequency_string = StringVar(root)
+        self.root.geometry("500x300")
+        self.frequency_string = StringVar(self.root)
 
-        self.ui_label = ttk.Label(frame, text="Autoclicker Program", font=15, background="#AFE1AF")
+        self.ui_label = ttk.Label(frame, text="Autoclicker Program", font=15)
         self.ui_label.grid(column=0, row=0)
         self.ui_entry = ttk.Entry(frame, textvariable=self.frequency_string)
-        self.ui_entry.grid(column=0, row=1, pady=50)
+        self.ui_entry.grid(column=0, row=1, pady=30)
         self.ui_runbutton = ttk.Button(frame, text="Run", command=self.get_frequency)
-        self.ui_runbutton.grid(column=0, row=3, pady=20)
-        root.columnconfigure(0, weight=1)
+        self.ui_runbutton.grid(column=0, row=2, pady=20)
+        self.ui_feedback_label = ttk.Label(frame, text="", font=15)
+        self.ui_feedback_label.grid(column=0, row=3, pady = 20)
+        self.root.columnconfigure(0, weight=1)
 
-        root.mainloop()
+        self.root.mainloop()
+
+    def stop_autoclicker(self):
+        self.root.destroy()
 
 
     
 def main():
     main_autoclicker = autoclicker()
+    main_autoclicker.create_window()
+
 
 
 
