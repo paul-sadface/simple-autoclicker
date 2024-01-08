5dd75f50437417a23f43b55dfd0d69bf192fc808
paul-sadface <paultriminh60@gmail.com>
2024-01-07 21:35:16 -0500

    attempt to finish the thing

--- autoclicker.py
+++ autoclicker.py
@@ -1,5 +1,4 @@
-import pyautogui
-from pynput import keyboard
+from pynput import keyboard, mouse
 from tkinter import *
 from tkinter import ttk
 import time
@@ -7,8 +6,12 @@ import time
 
 class autoclicker():
     def __init__(self):
-        self.is_running = False
+        self.is_running = True
+        self.is_paused = True
         self.root = None
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.mouse_controller = mouse.Controller()
+        
 
     def get_frequency(self):
         self.frequency = self.frequency_string.get()
@@ -31,28 +34,24 @@ class autoclicker():
         self.ui_feedback_label = ttk.Label(frame, text="", font=15)
         self.ui_feedback_label.grid(column=0, row=3, pady = 20)
         self.root.columnconfigure(0, weight=1)
+        self.root.protocol("WM_DELETE_WINDOW", self.stop_autoclicker)
 
         self.root.mainloop()
 
     def stop_autoclicker(self):
         self.root.destroy()
+        self.listener.stop()
     
     def on_press(self, key):
         if key == keyboard.Key.f1:
             print("pressed f1")
+            self.mouse_controller.click(mouse.Button.left, 1)
+            
 
-
-
-    
 def main():
     main_autoclicker = autoclicker()
-    listener = keyboard.Listener(on_press=main_autoclicker.on_press)
-    listener.start()
+    main_autoclicker.listener.start()
     main_autoclicker.create_window()
-    
-
-
-
 
 
 
